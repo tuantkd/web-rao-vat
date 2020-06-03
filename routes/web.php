ff12c6336166726655237b16677b9d7a7df47d4b
Tuan Nguyen <nguyenvantuan9a7@gmail.com>
2020-06-03 09:50:26 +0700

    them moi

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*HOME*/
+//Trang chủ
 Route::get('/', 'HomeController@index');
+
+//Xem danh mục
+Route::get('view-category', 'HomeController@view_category');
+
+//Xem danh mục chi tiết
+Route::get('view-category-detail', 'HomeController@view_category_detail');
+
+//Báo cáo vi phạm bản tin
+Route::get('report-new', 'HomeController@report_new');
